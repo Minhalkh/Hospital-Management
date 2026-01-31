d29754c3025cf7faf33eec293f223c5751597681
MinhalKh <javaidminhal1@gmail.com>
2026-01-31 22:18:09 +0500

    Contact Page completed

--- src/app/Pages/About/page.jsx
+++ src/app/Pages/About/page.jsx
@@ -12,14 +12,19 @@ function page() {
       <div>
       <h1 className='text-4xl'>GLOBAL HOSPITALS</h1>
       <p className='text-3xl mt-2'>Provide best quality health care for you</p>
+
       <div className='mt-12'><h3 className='font-bold text-xl'>Affordable monthly premium packages</h3>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p></div>
+
       <div className='my-4'><h3 className='font-bold text-xl'>Choose Your Favourite Services</h3>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p></div>
+
       <div><h3 className='font-bold text-xl'>Only Use Friendly Environment</h3>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p></div>
+
       <button className='bg-white text-blue-700 ml-77 mt-7 p-2 rounded'>LEARN MORE
       </button>
+      
       </div>
       <div>
         <Image src={img1} alt='doctor' className='w-[565px]'/>
--- src/app/Pages/Contact/page.jsx
+++ src/app/Pages/Contact/page.jsx
@@ -3,9 +3,32 @@ import React from 'react'
 function page() {
   return (
     <div className='mx-[240px] my-[90px] w-[900px] rounded-l-[100px] rounded-r-[100px] h-[430px] bg-white'>
-      <div className='mx-[340px]'>
+      <div className='ml-[340px]'>
         <h1 className=''>Drop Us a Message</h1>
         </div>
+        <div className='flex justify-around items-center mt-20 mx-20'>
+        <div className='grid grid-cols-1 gap-4'>
+        <input type="text"
+        name='name'
+        placeholder='Your Name*'
+        className='border rounded-full p-1' />
+
+        <input type="text"
+        name="name" 
+        placeholder='Your Email*' 
+        className='border rounded-full p-1'/>
+
+        <input type="number" name="name" 
+        placeholder='Your Phone Number*' 
+        className='border rounded-full p-1'/>
+        </div>
+        <div>
+          <input type="text" name='name'
+          placeholder='Your Message*' 
+          className='border rounded w-[290px] h-[180px]'/>
+        </div>
+        </div>
+        <button className='w-[140px] h-[39px] font-bold ml-[370px] mt-8 bg-blue-700 text-white border rounded-full'>Send Message</button>
     </div>
   )
 }
